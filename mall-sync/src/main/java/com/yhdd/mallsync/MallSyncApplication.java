4d27e85b7e1477897e6b9119301bcb02dcc137e2
xiaojunfeng <xiaojunfeng@batmobi.net>
2018-12-11 18:21:58 +0800

    Initial commit

---
+++ mall-sync/src/main/java/com/yhdd/mallsync/MallSyncApplication.java
@@ -0,0 +1,13 @@
+package com.yhdd.mallsync;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSyncApplication.class, args);
+    }
+}
